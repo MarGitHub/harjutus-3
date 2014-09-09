d780a6a74046b0c8b8b209daba3a7c99c22b45a8
Margit <margit.pilden@gmail.com>
2014-09-09 13:09:06 +0300

    Add sample variables

---
+++ index.php
@@ -0,0 +1,26 @@
+<! DOCKTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>
+	harjtus-3s - Muutujad
+	</title>
+</head>
+
+<body>
+	<?php
+    $variable = 15;
+    echo $variable;
+       echo "<br>";
+	   $variable = 20;
+    echo $variable;
+?>
+
+<?php
+    $variable = 45;
+    echo $variable;
+?>
+
+</body>
+
+</html>
