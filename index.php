bc19ab82eeef1e871168b567023796fd8087e812
Margit <margit.pilden@gmail.com>
2014-09-09 13:40:10 +0300

    Add my name variables

--- index.php
+++ index.php
@@ -3,23 +3,67 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title>
-	harjtus-3s - Muutujad
+	harjtus-3s - Andmetüübid
 	</title>
 </head>
 
 <body>
+	
+<h1>Muutujad</h1>	
 	<?php
     $variable = 15;
     echo $variable;
        echo "<br>";
-	   $variable = 20;
-    echo $variable;
 ?>
 
 <?php
     $variable = 45;
     echo $variable;
 ?>
+
+<h2>Stringid</h2>
+
+<?php echo "Hello world!"; ?> 
+<?php echo 'Hello world!'; ?> 
+<br>
+
+<?php $variable = "24"; 
+	$variable2 = "26"; 
+echo ($variable . $variable2);
+	?>
+	
+<h2>Nimekirja väljastamine muutujate ja stringide abil</h2>
+
+<?php 
+	$student1 = "Ants"; 
+	$student2 = "Maile"; 
+	$student3 = "Syret"; 
+	$student4 = "Fränk"; 
+	$student5 = "Aigar"; 
+?>
+
+<ul>
+<?php 
+	echo "<li>". $student1 . "</li>
+	<li>" . $student2 . "</li>
+	<li>" .$student3 . "</li>
+	<li>" .$student4 . "</li>
+	<li>" .$student5 . "</li>"
+?>
+</ul>
+
+<?php 
+	$firstName ="Margit ";
+	$lastName = "Pilden";
+	$fullName = $firstName.$lastName; 
+	echo $fullName;
+	?>
+	
+	
+
+
+
+
 
 </body>
 
